f493ca74870daa58663b6b2d488dabffaded0405
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2023-08-29 13:36:57 -0300

    feat: improve button

--- src/components/common/button/buttton.tsx
+++ src/components/common/button/buttton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiOutlineLoading } from 'react-icons/ai'
+import { AiOutlineLoading } from 'react-icons/ai';
 
 type ButtonProps = {
   text: string;
@@ -7,21 +7,40 @@ type ButtonProps = {
   text_color?: string;
   width?: string;
   border_radius?: string;
-  loading: boolean;
+  loading?: boolean;
   error?: string;
   clickFunction: () => void;
   icon?: React.ReactNode;
 };
 
-const ButtonComponent: React.FC<ButtonProps> = ({ text, color = "bg-azul_escuro", text_color = "text-branco", width = "w-full",
-  border_radius = "", loading, error, clickFunction, icon }) => {
+const ButtonComponent: React.FC<ButtonProps> = ({
+  text,
+  color = "bg-azul_escuro",
+  text_color = "text-branco",
+  width = "w-full",
+  border_radius = "",
+  loading = false,
+  error,
+  clickFunction,
+  icon
+}) => {
+  const renderIconOrLoading = () => {
+    if (loading) {
+      return <span className='animate-spin'><AiOutlineLoading color="white"/></span>;
+    }
+    if (icon && !loading) {
+      return icon;
+    }
+    return null;
+  };
+
   return (
     <div className={`flex flex-col gap-2 items-start ${width} flex-grow-0 flex-shrink-0`}>
       <button
         className={`flex flex-row gap-2 items-center justify-center ${width} ${border_radius} ${color} cursor-pointer`}
         onClick={clickFunction}>
         <p className={`font-medium ${text_color} py-2`}>{text}</p>
-        {loading && <span className='animate-spin'><AiOutlineLoading color="white"/></span>}
+        {renderIconOrLoading()}
       </button>
       {error && <p className="text-vermelho text-xs mt-1">{error}</p>}
     </div>
