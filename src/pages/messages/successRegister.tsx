ad267e9e68b28d7caf4a4b4e86ac4017a1481163
alex81632 <alexandrebellargus@hotmail.com>
2023-08-30 12:00:33 -0300

    add successRegister message

---
+++ src/pages/messages/successRegister.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import MessagesLayout from '~/layouts/messagesLayout';
+
+const LoginPage = () => {
+
+    const router = useRouter();
+
+    const onSubmit = async () => {
+        router.push("/auth/signin")
+    };
+
+    return (
+        <MessagesLayout image="/assets/festivities.svg" width={500} height={292} buttonOnClickFunc={onSubmit} buttonText='Fazer Login' message='O cadastro foi um sucesso'>
+            
+        </MessagesLayout>
+    );
+};
+
+export default LoginPage;
