6438a7ca9300f9f19f58ab92f84e0b371432b2b2
DeodatoBastos <deodatobasto@gmail.com>
2023-11-02 16:43:56 -0300

    add chart component

--- src/pages/dashboard/overview.tsx
+++ src/pages/dashboard/overview.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import DashboardLayout from '~/layouts/dashboard/dashboardLayout';
-import Demand from '~/components/dashboard/overview/demand';
-import InvOpt from '~/components/dashboard/overview/invOpt';
-import Progress from '~/components/dashboard/overview/progress';
+import React from "react";
+import DashboardLayout from "~/layouts/dashboard/dashboardLayout";
+import Demand from "~/components/dashboard/overview/demand";
+import InvOpt from "~/components/dashboard/overview/invOpt";
+import Progress from "~/components/dashboard/overview/progress";
 
-
-const Overview: React.FC = ({ }) => {
+const Overview: React.FC = ({}) => {
+    const data = [
+        { x: 1, y: 400 },
+        { x: 2, y: 378 },
+        { x: 3, y: 399 },
+        { x: 4, y: 398 },
+        { x: 5, y: 356 },
+        { x: 6, y: 402 },
+        { x: 6, y: 366 },
+    ];
 
     return (
         <DashboardLayout>
             <Progress />
-            <div className='flex flex-row justify-start items-start w-full gap-2.5 bg-branco'>
-                <Demand />
+            <div className="flex w-full flex-row items-start justify-start gap-2.5 bg-branco">
+                <Demand data={data} />
                 <InvOpt />
             </div>
         </DashboardLayout>
